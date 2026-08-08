33fcb2a2472234442e56d2281e3694d069737dad
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-09 01:03:59 +0530

    added models and wired them in the admin panel in teams app

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from teams.models import Team, Member
 
-# Register your models here.
+admin.site.register(Team)
+admin.site.register(Member)
--- teams/models.py
+++ teams/models.py
@@ -1,2 +1,24 @@
 from django.db import models
+from project.models import Project
+from register.models import Profile
 
+class Team(models.Model):
+    project=models.ManyToManyField(Project)
+    name=models.CharField(max_length=100)
+    specialization=models.CharField(max_length=300)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Member(models.Model):
+    team=models.ForeignKey(Team, on_delete=models.CASCADE)
+    user=models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name=models.CharField(max_length=150)
+    description=models.TextField(null=True)
+    is_leader=models.BooleanField(default=False)
+    added_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.user.username} of {self.team.name}"
