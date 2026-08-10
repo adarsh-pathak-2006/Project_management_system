a499af65d5cadc6759fddc2741256209d8c2807e
duckyduck01 <madhvetipathak2006@gmail.com>
2026-08-10 14:45:13 +0530

    Add /docs/ URL alias for Swagger UI

--- task_management/urls.py
+++ task_management/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('docs/', SpectacularSwaggerView.as_view(url_name='schema')),
     path('api/auth/', include('register.urls')),
     path('api/projects/', include('project.urls')),
     path('api/teams/', include('teams.urls')),
